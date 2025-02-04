bb4aadacdc3933b92aae790b3e3d283af001e857
Kim Beomhee <nakaldi@gmail.com>
2025-02-04 16:45:51 +0900

    Add Manager model

---
+++ src/kims/semi1/model/Manager.java
@@ -0,0 +1,23 @@
+package kims.semi1.model;
+
+public class Manager {
+	private int managerId;
+	private String password;
+
+	public int getManagerId() {
+		return managerId;
+	}
+
+	public void setManagerId(int managerId) {
+		this.managerId = managerId;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+}
